71edbcbda1cc94c70b146e6f2c600170374d32ba
Johnkideys <74567050+Johnkideys@users.noreply.github.com>
2021-10-27 21:42:46 +0100

    added a config.py file where we defined the default configuration of our file

--- FlaskBlog/config.py
+++ FlaskBlog/config.py
@@ -1,1 +1,24 @@
-class Config:  
+import os
+
+from pathlib import Path  # python3 only
+from dotenv import load_dotenv
+from dotenv.main import find_dotenv
+#load_dotenv(find_dotenv()) #this line only finds .env but doesnt find .flaskenv
+
+env_path1 = Path('/Users/AhmetKideys/Desktop/Programming/GitHub/FlaskPolitoGithubFolder/.flaskenv') 
+load_dotenv(dotenv_path=env_path1)
+
+env_path2 = Path('/Users/AhmetKideys/Desktop/Programming/GitHub/FlaskPolitoGithubFolder/.env') 
+load_dotenv(dotenv_path=env_path2)
+
+class Config:
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+
