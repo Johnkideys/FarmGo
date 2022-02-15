b4e9002598c7b253b53657af09be1664341cd8df
John Kideys <johnkideys@gmail.com>
2022-02-15 10:11:35 +0000

    deleted unnecessary comments, class Config is here with environment variables getting their value, example: os.environ.get('EMAIL_USER'), we have created a .env file which will be in .gitignore, thanks to python-dotenv os.environ.get can automatically detect .env

--- FlaskBlog/config.py
+++ FlaskBlog/config.py
@@ -1,16 +1,5 @@
 import os
 
-from pathlib import Path  # python3 only
-from dotenv import load_dotenv
-from dotenv.main import find_dotenv
-#load_dotenv(find_dotenv()) #this line only finds .env but doesnt find .flaskenv
-
-env_path1 = Path('/Users/AhmetKideys/Desktop/Programming/GitHub/FlaskPolitoGithubFolder/.flaskenv') 
-load_dotenv(dotenv_path=env_path1)
-
-env_path2 = Path('/Users/AhmetKideys/Desktop/Programming/GitHub/FlaskPolitoGithubFolder/.env') 
-load_dotenv(dotenv_path=env_path2)
-
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
